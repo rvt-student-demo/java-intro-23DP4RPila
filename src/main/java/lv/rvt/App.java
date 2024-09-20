fe89387bd684200df27a874b90f68ea45288becc
23DP4RPila <166484724+23DP4RPila@users.noreply.github.com>
2024-09-20 10:47:17 +0000

    Simple calculator in java!

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,35 @@
-package lv.rvt;
+package lv.rvt; // atrašanās vieta
+
+import java.util.*;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        // Šeit būs mans komentārs
-        System.out.println("Hello, world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number: ");
+        Double firstNumber = Double.valueOf(scanner.nextLine());
+        
+        System.out.println("Give the second number: ");
+        Double secondNumber = Double.valueOf(scanner.nextLine());
+
+        Double sumResult = firstNumber + secondNumber;
+
+        System.out.println(firstNumber + " + " + secondNumber + " = " + sumResult);
+        
+        Double substractResult = firstNumber - secondNumber;
+
+        System.out.println(firstNumber + " - " + secondNumber + " = " + substractResult);
+        
+        Double multiplyResult = firstNumber * secondNumber;
+
+        System.out.println(firstNumber + " * " + secondNumber + " = " + multiplyResult);
+        
+        Double divideResult = firstNumber / secondNumber;
+
+        System.out.println(firstNumber + " / " + secondNumber + " = " + divideResult);
+        
+
     }
-    // 
 }
