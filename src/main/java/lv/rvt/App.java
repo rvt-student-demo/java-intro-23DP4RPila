22a0a2cd9b6af834b155cb3d904f1dcbe174b907
23DP4RPila <166484724+23DP4RPila@users.noreply.github.com>
2024-09-27 10:43:52 +0000

    points evalutator

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,16 +6,29 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Give speed:");
+        System.out.println("Give points [0-100]:");
         Scanner scanner = new Scanner(System.in);
 
         String input = scanner.nextLine();
 
         int result = Integer.valueOf(input);
 
-        if (result > 120) {
-            System.out.println("Speeding ticket!");
+        if (result < 0 ) {
+            System.out.println("impossible!");
+        } else if (result < 49) {
+            System.out.println("failed");
+        } else if (result < 59) {
+            System.out.println("1");
+        } else if (result < 69) {
+            System.out.println("2");
+        } else if (result < 79) {
+            System.out.println("3");
+        } else if (result < 89) {
+            System.out.println("4");
+        } else if (result < 100) {
+            System.out.println("5");
+        } else if (result > 100) {
+            System.out.println("incredible!");
         }
-
     }
 }
