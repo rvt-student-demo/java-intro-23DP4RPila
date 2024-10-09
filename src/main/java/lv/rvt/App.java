e64f0853224d68d5eab4eb55cc52083dfb5d9135
23DP4RPila <166484724+23DP4RPila@users.noreply.github.com>
2024-10-09 17:07:12 +0000

    only positives - kāpināt tikai ar pozitivajiem skaitļiem

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,25 +6,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Value of the gift?");
-        Scanner scanner = new Scanner(System.in);
 
+    Scanner scanner = new Scanner(System.in);
+
+    while (true) {
+        System.out.println("Give a number:");
         String input = scanner.nextLine();
+        int number = Integer.valueOf(input);
 
-        Double value = Double.valueOf(input);
-
-        if (value < 5000 ) {
-            System.out.println("No tax\t!");
-        } else if (value < 25000) {
-            System.out.println("Tax: " + (100 + (value - 5000) * 0.08));
-        } else if (value < 55000) {
-            System.out.println("Tax: " + (1700 + (value - 25000) * 0.10));
-        } else if (value < 200000) {
-            System.out.println("Tax: " + (4700 + (value - 55000) * 0.12));
-        } else if (value < 1000000) {
-            System.out.println("Tax: " + (22100 + (value - 200000) * 0.15));
-        } else if (value > 1000000) {
-            System.out.println("Tax: " + (142100 + (value - 1000000) * 0.17));
+        if (number < 0) {
+            System.out.println("Unsuitable number");
+        } else if (number == 0) {
+            break;
+        } else if (number > 0) {
+            System.out.println(number * number);
         }
     }
+}
 }
