af618b40f9e0c8d7fb52137b66cd318c4c12fd8d
23DP4RPila <166484724+23DP4RPila@users.noreply.github.com>
2024-10-11 11:05:52 +0000

    Reprint

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,34 +9,18 @@ public class App
 
     Scanner scanner = new Scanner(System.in);
 
-    int numberSum = 0;
-    int numberAmount = 0;
-    int evenNumbers = 0;
-    int oddNumbers = 0;
-
-
-    while (true) {
-        System.out.println("Write numbers:");
-        String stringInput = scanner.nextLine();
-        int userInput = Integer.valueOf(stringInput);
-       
-        if (userInput == -1) {
-            System.out.println("Thx! Bye!");
-            break;
-        } else {
-            numberSum += userInput;
-            numberAmount++;
-        } if (userInput % 2 == 0) {
-            evenNumbers++;
-        } if (!(userInput % 2 == 0)) {
-            oddNumbers++;
-        }
+    System.out.println("How many times?: ");
+    String stringInput = scanner.nextLine();
+    int userInput = Integer.valueOf(stringInput);
+    int times = 1;
+    
+    while (times <= userInput) {
+        greetings();
+        times++;   
+    }
     }
 
-    System.out.println("Sum: " + numberSum);
-    System.out.println("Numbers:" + numberAmount);
-    System.out.println("Average: " + (Double.valueOf(numberSum) / numberAmount));
-    System.out.println("Even: " + evenNumbers);
-    System.out.println("Odd: " + oddNumbers);
+    public static void greetings() {
+        System.out.println("Labdien!");
     }
 }
