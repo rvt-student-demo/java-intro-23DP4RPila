37007fa443b1a09c7fa84737e2a6d26873487ef6
23DP4RPila <166484724+23DP4RPila@users.noreply.github.com>
2024-10-31 13:16:54 +0000

    Arrays task 2

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,12 +4,20 @@ public class App
 {
     public static void main( String[] args )
     {          
-    int[] val = {0, 1, 2, 3}; 
-
-    int sum = val[0] + val[1] + val[2]+ val[3];
-
-    System.out.println( "Sum of all numbers = " + sum );
+        int[] val = {13, -4, 82, 17}; 
+        int[] twice = new int[4];  // [0, 0, 0, 0]
+        
+        
+        System.out.println( "Original Array: " 
+            + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+     
+        for (int i = 0; i < val.length; i++) {
+            twice[i] = val[i] *= 2;  // val[i] = val[i] * 2
+        
+        }
     
+     
+        System.out.println( "New Array: " + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
     }
 
 }
