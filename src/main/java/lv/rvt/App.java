501c60e646cefb0ddea85e8520103b9e683cf105
23DP4RPila <166484724+23DP4RPila@users.noreply.github.com>
2024-12-06 07:46:38 +0000

    PersonManager (part 1)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,26 +1,18 @@
 package lv.rvt; // atrašanās vieta
 
+import java.io.BufferedReader;
+import java.util.Scanner;
+
 public class App 
-{        public static void main(String[] args) {
-   
-    HealthStation childrensHospital = new HealthStation();
+{
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        BufferedReader reader = Helper.getReader("persons.csv");
 
-    Person ethan = new Person("Ethan", 1, 110, 7);
-    Person peter = new Person("Peter", 33, 176, 85);
+        System.out.println(reader.readLine());
+        System.out.println(reader.readLine());
+        System.out.println(reader.readLine());
+        System.out.println(reader.readLine());
 
-    System.out.println("weighings performed: " + childrensHospital.weighings());
-
-    childrensHospital.weight(ethan);
-    childrensHospital.weight(peter);
-
-    System.out.println("weighings performed: " + childrensHospital.weighings());
-
-    childrensHospital.weight(ethan);
-    childrensHospital.weight(ethan);
-    childrensHospital.weight(ethan);
-    childrensHospital.weight(ethan);
-
-    System.out.println("weighings performed: " + childrensHospital.weighings());
-        
     }
 }
---
+++ src/main/java/lv/rvt/PersonManager.java
@@ -0,0 +1,9 @@
+package lv.rvt;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class PersonManager {
+    public static void getPersonList() throws Exception {
+     //   ArrayList<PersonManager> = new ArrayBlockingQueue<>(0)
+    }
+}
